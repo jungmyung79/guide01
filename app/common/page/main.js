899c81a40dff6bf6eb423d344357d904626af349
seo <seo@seoui-MacBook-Pro.local>
2015-10-27 16:13:56 +0900

    [feature] add Main.js

---
+++ app/common/page/main.js
@@ -0,0 +1,20 @@
+
+/**
+ * 메인 페이지 스크립트 파일
+ *
+ *
+ *
+ * @author jungmyung.seo@yt.com
+ * @version 1.0
+ * @since 2015-10-27
+ *
+ *
+ *
+ * @class
+ * @ignore
+ */
+var ns = require("../namespace.js");
+
+ns.Main = {
+
+};
